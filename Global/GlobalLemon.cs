6a09372aa19f941e00949f6e063fa4e07a8449e6
Raoul1808 <raoulthegeek@gmail.com>
2023-08-04 14:06:48 +0200

    tModLoader 1.4.4 port + new death strings

--- Global/GlobalLemon.cs
+++ Global/GlobalLemon.cs
@@ -1,18 +1,23 @@
+using System;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Lemon.Global;
 
 public class GlobalLemon : GlobalItem
 {
+    private const int LemonDeathMessageCount = 3;
+    
     public override bool ConsumeItem(Item item, Player player)
     {
         switch (item.type)
         {
             case ItemID.Lemon:
-                player.KillMe(PlayerDeathReason.ByCustomReason(player.name + " ate a lemon and died."), 727, 0);
+                string deathMsg = Language.GetText("Mods.Lemon.GlobalLemon.LemonDeath" + (Random.Shared.Next(LemonDeathMessageCount) + 1)).WithFormatArgs(player.name).Value;
+                player.KillMe(PlayerDeathReason.ByCustomReason(deathMsg), 727, 0);
                 return true;
             
             default:
